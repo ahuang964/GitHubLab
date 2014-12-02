de61276c67b2715e6abcd8c2a5b6ff6c75526df0
Alan Huang <ahuang@paypoint.com>
2014-12-02 15:08:18 -0800

    add files that i missed from previous checkin

---
+++ Exercise3-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 42;
+            float b = (float)98.6;
+            double c = 12345.6789;
+            char d = 'Z';
+            string e = "The quick brown fox jumped over the lazy dogs";
+
+            Console.WriteLine("int: {0}\nfloat: {1}\ndouble: {2}\nchar: {3}\nstring: {4}", a, b, c, d, e);
+        }
+    }
+}
