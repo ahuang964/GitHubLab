d638ba83b4471c232e04c677fc8f3805a4b683a8
Alan Huang <ahuang@paypoint.com>
2014-12-02 17:14:02 -0800

    exercise 4-1

---
+++ Exercise4-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            int y = 5;
+
+            Console.WriteLine("Sum: {0}\nDiff: {1}\nProduct: {2}\nQuotient: {3}\nMod: {4}",x+y, x-y, x*y, x/y, x%y);
+        }
+    }
+}
