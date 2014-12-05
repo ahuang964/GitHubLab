ac782ec60b9e3359e6f37ab3f564efc13c9365e9
Alan Huang <ahuang@paypoint.com>
2014-12-04 16:33:57 -0800

    commiting exercise 5-3

---
+++ Exercise5-3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer:");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+
+            switch (inputNum)
+            {
+                case 0:
+                    Console.WriteLine("You entered 0");
+                    break;
+            }
+
+            int mod2 = inputNum%2;
+            switch (mod2)
+            {
+                case 0:
+                    Console.WriteLine("You entered even number");
+                    break;
+                case 1:
+                    Console.WriteLine("You enetered odd number");
+                    break;
+            }
+
+            int mod10 = inputNum%10;
+            switch (mod10)
+            {
+                case 0:
+                    Console.WriteLine("You entered multiple of 10");
+                    break;
+            }
+
+            bool toLarge = inputNum > 100 ? true : false;
+            switch (toLarge)
+            {
+                case true:
+                    Console.WriteLine("The number you entered is too large");
+                    break;
+            }
+        }
+    }
+}
