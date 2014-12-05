62b2f04b8a60e1a82e46210b60a6e4238b906aa0
Alan Huang <ahuang@paypoint.com>
2014-12-04 16:38:49 -0800

    commiting exercise 5-4

---
+++ Exercise5-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 25;
+            while (i <= j)
+            {
+                Console.WriteLine("i = {0} AND j = {1}", i, j);
+                i++;
+                j--;
+            }
+            Console.WriteLine("Crossed over!");
+        }
+    }
+}
