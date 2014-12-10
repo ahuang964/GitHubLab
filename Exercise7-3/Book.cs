322caea7fe45476bd6438b25acaaed78be60a9c1
Alan Huang <ahuang@paypoint.com>
2014-12-10 15:48:39 -0800

    commiting exercise7-3

---
+++ Exercise7-3/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7_3
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public string Publisher;
+        public string ISBN;
+
+        public Book(string title, string author, string isbn)
+        {
+            Title = title;
+            Author = author;
+            Publisher = "O'Reilly";
+            ISBN = isbn;
+        }
+
+        public void DisplayBook()
+        {
+            Console.WriteLine("Book info:\nTitle: {0}\nAuthor: {1}\nPublisher: {2}\nISBN: {3}", Title, Author, Publisher, ISBN);
+        }
+    }
+}
---
+++ Exercise7-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Programming C# 3.0", "Jesse Liberty and Donald Xie", "9780596527433");
+            Book book2 = new Book("C# 3.0 In a Nutshell", "Joseph Albahari and Ben Albahari", "9780596527570");
+            Book book3 = new Book("C# 3.0 Cookbook", "Jay Hilyard and Stephen Teilhet", "9780596516109");
+
+            book1.DisplayBook();
+            book2.DisplayBook();
+            book3.DisplayBook();
+        }
+    }
+}
