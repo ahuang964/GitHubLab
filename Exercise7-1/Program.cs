2ea21009da67747374947c3d214f695b11857eb4
Alan Huang <ahuang@paypoint.com>
2014-12-10 15:27:05 -0800

    commiting modifications for exercise7-1

--- Exercise7-1/Program.cs
+++ Exercise7-1/Program.cs
@@ -10,32 +10,31 @@ namespace Exercise7_1
     {
         static void Main(string[] args)
         {
-            Math myMath = new Math();
-            myMath.Add(3,6);
-            myMath.Subtract(6,3);
-            myMath.Multiply(2,5);
-            myMath.Divide(100,2);
+            Math.Add(3,6);
+            Math.Subtract(6,3);
+            Math.Multiply(2,5);
+            Math.Divide(100,2);
         }
     }
 
     class Math
     {
-        public void Add(double a, double b)
+        public static void Add(double a, double b)
         {
             Console.WriteLine("{0} + {1} = {2}", a, b, a+b);
         }
 
-        public void Subtract(double a, double b)
+        public static void Subtract(double a, double b)
         {
             Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
         }
 
-        public void Multiply(double a, double b)
+        public static void Multiply(double a, double b)
         {
             Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
         }
 
-        public void Divide(double a, double b)
+        public static void Divide(double a, double b)
         {
             Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
         }
