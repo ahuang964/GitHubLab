f6c74534a6ce78eb89dd73df0cec5c930ec4c364
Alan Huang <ahuang@paypoint.com>
2014-12-10 15:20:03 -0800

    commiting exercise7-1

---
+++ Exercise7-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math myMath = new Math();
+            myMath.Add(3,6);
+            myMath.Subtract(6,3);
+            myMath.Multiply(2,5);
+            myMath.Divide(100,2);
+        }
+    }
+
+    class Math
+    {
+        public void Add(double a, double b)
+        {
+            Console.WriteLine("{0} + {1} = {2}", a, b, a+b);
+        }
+
+        public void Subtract(double a, double b)
+        {
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+        }
+
+        public void Multiply(double a, double b)
+        {
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+        }
+
+        public void Divide(double a, double b)
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+        }
+    }
+}
