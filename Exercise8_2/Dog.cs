ab5728154adeab5eed92abee2500416fcc28735b
Alan Huang <ahuang@paypoint.com>
2014-12-11 10:10:38 -0800

    commiting exercise8-2

---
+++ Exercise8_2/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8_2
+{
+    class Dog
+    {
+        public double Weight { get; set; }
+        public string Color { get; set; }
+
+        public Dog(string color)
+        {
+            Color = color;
+        }
+    }
+}
---
+++ Exercise8_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("white");
+
+            Console.WriteLine("My dog has color {0}", myDog.Color);
+            Console.WriteLine("Please enter dog's weight:");
+            string weight = Console.ReadLine();
+            myDog.Weight = Convert.ToDouble(weight);
+            Console.WriteLine("My dog is weighted {0}", myDog.Weight);
+        }
+    }
+}
