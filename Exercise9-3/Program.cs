ac869e9438e6e0b52c44f665d9f0a558639309c7
Alan Huang <ahuang@paypoint.com>
2014-12-11 11:34:12 -0800

    commiting exercise9-3

---
+++ Exercise9-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Exercise9_3
+{
+    class Program
+    {
+        public static int Factorial(int myInt)
+        {
+            int result = 1;
+            for (int i = 1; i <= myInt; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+        static void Main()
+        {
+            int input = 5;
+            Console.WriteLine("{0} factorial is {1}",
+            input, Factorial(input));
+        }
+    }
+}
