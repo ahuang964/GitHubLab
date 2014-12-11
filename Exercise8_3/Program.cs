165270831dca1676482d2362fe16d898de24cb0c
Alan Huang <ahuang@paypoint.com>
2014-12-11 10:19:38 -0800

    commiting exercise8-3

---
+++ Exercise8_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8_3
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            Console.WriteLine("Enter a number:");
+            double gg = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Double of your input is: {0}\nTriple of your input is: {1}", gg*2, gg*3);
+        }
+    }
+}
