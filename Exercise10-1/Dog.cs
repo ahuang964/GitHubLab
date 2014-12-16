7d636107534b00020d53f8c430e2bc004d0cc885
Alan Huang <ahuang@paypoint.com>
2014-12-16 11:12:06 -0800

    commiting exercise10-1

---
+++ Exercise10-1/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10_1
+{
+    class Dog
+    {
+        private int weight { get; set; }
+        private string name { get; set; }
+
+        public Dog(int weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine("Dog name: {0}; Dog weight: {1} pounds", name, weight);
+        }
+    }
+}
---
+++ Exercise10-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog[] dogs = new Dog[3];
+            dogs[0] = new Dog(26, "Milo");
+            dogs[1] = new Dog(10, "Frisky");
+            dogs[2] = new Dog(50, "Laika");
+
+            for (int i = 0; i < dogs.Length; i++)
+            {
+                dogs[i].Output();
+            }
+        }
+    }
+}
