d3b758fc45273d32e5ace0e87c560db66bee13b5
Alan Huang <ahuang@paypoint.com>
2014-12-16 15:50:06 -0800

    commiting exercise10-3

---
+++ Exercise10-3/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10_3
+{
+    class Dog
+    {
+        private int weight { get; set; }
+        private string name { get; set; }
+        private string[] awards { get; set; }
+
+
+        public Dog(int weight, string name, string[] awards)
+        {
+            this.weight = weight;
+            this.name = name;
+            this.awards = awards;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine("Dog name: {0}; Dog weight: {1} pounds", name, weight);
+            Console.WriteLine("Awards won:");
+            foreach (string str in awards)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ Exercise10-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] awards1 = {"award1", "award2"};
+            string[] awards2 = { "award3", "award4", "award5"};
+            string[] awards3 = { "award6", "award7", "award8", "award9" };
+
+            Dog[] dogs = new Dog[3];
+            dogs[0] = new Dog(26, "Milo", awards1);
+            dogs[1] = new Dog(10, "Frisky", awards2);
+            dogs[2] = new Dog(50, "Laika", awards3);
+
+            for (int i = 0; i < dogs.Length; i++)
+            {
+                dogs[i].Output();
+            }
+        }
+    }
+}
