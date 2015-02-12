c8c37dfaa5299ade56daf4b13f6dc762f37f5ac5
Alan Huang <ahuang@paypoint.com>
2015-02-11 17:05:53 -0800

    commiting Exercise 11-1

---
+++ Exercise11-1/Telephone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11_1
+{
+    class Telephone
+    {
+        protected string phoneType;
+
+        public void Ring()
+        {
+            Console.WriteLine("Ringing the "+phoneType);
+        }
+    }
+}
